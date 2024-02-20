d6051dc53546d57b45bb9cbbfc5ee3c07b46e39a
Balázs Hunor <hunorgaming000@gmail.com>
2024-02-20 14:54:04 +0100

    Random

--- src/main/java/com/example/vadasz/HelloController.java
+++ src/main/java/com/example/vadasz/HelloController.java
@@ -16,9 +16,14 @@ public class HelloController {
     public Image[] icons = new Image[5];
 
     public final int DARK = 0;
+    public final int ROKA = 2;
+    public final int TREE = 4;
 
     public ImageView[][] it = new ImageView[16][32];
     public int[][] t = new int[16][32];
+
+    public int roka;
+    public int rokaMax;
 
     public void initialize() {
         for (int i = 0; i < 5; i++) icons[i] = new Image(getClass().getResourceAsStream(iconNev[i] + ".png"));
@@ -28,5 +33,15 @@ public class HelloController {
             it[y][x].setLayoutY(10+y*48);
             pane.getChildren().addAll(it[y][x]);
         }
+        generalErdo();
+    }
+
+    public void generalErdo() {
+        roka = 0;
+        for (int y = 0; y < 16; y++) for (int x = 0; x < 32; x++) {
+            if (Math.random() < 0.1) { t[y][x] = ROKA; roka++;} else t[y][x] = TREE;
+        }
+        rokaMax = roka;
+        lbRoka.setText(roka + " / " + rokaMax + " róka");
     }
 }
